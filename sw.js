5292aec3e7b7ab18501fd46efb81c1b62d664e2c
candidaperez38-ai <candidaperez38@gmail.com>
2025-07-30 15:49:10 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'a9-redeem-cache-v1';
+const filesToCache = [
+  './redeem-helper-pwa.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
